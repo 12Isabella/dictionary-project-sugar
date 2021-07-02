ba60d9bdcee40d4f217ca8893f4e7dcac34714f6
12Isabella <81431189+12Isabella@users.noreply.github.com>
2021-07-02 10:56:28 +0200

    creating react dictionary app, installing bootstrap

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <div className="container">
+      <p>Hello</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
