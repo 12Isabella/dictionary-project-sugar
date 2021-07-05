0896ee9f2ed657e22fc6f0c1a893bfdd6b35af9c
12Isabella <81431189+12Isabella@users.noreply.github.com>
2021-07-05 18:10:14 +0200

    fixed the loop

--- src/Meaning.jsx
+++ src/Meaning.jsx
@@ -5,7 +5,7 @@ export default function Meaning(props) {
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
-      {props.meaning.definitions.map(function (index, definition) {
+      {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
             <p>
