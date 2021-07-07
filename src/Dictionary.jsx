452700e3012ac32595f1d556f82cb06f35eb1167
12Isabella <81431189+12Isabella@users.noreply.github.com>
2021-07-07 12:57:29 +0200

    set default keyword to show on loading

--- src/Dictionary.jsx
+++ src/Dictionary.jsx
@@ -4,36 +4,54 @@ import "./Dictionary.css";
 import Results from "./Results";
 
 export default function Dictionary() {
-  let [input, setInput] = useState("");
+  let [input, setInput] = useState("hippopotamus");
   let [results, setResults] = useState(null);
+  let [loaded, setLoaded] = useState(false);
 
   function handleResponse(response) {
     setResults(response.data[0]);
   }
 
-  function handleSubmit(event) {
-    event.preventDefault();
-
+  function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${input}`;
     axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
   }
 
   function handleChange(event) {
     setInput(event.target.value);
   }
 
-  return (
-    <div className="Dictionary">
-      <section>
-        <form onSubmit={handleSubmit}>
-          <input
-            onChange={handleChange}
-            type="search"
-            placeholder="Enter your hopes and dreams..."
-          />
-        </form>
-      </section>
-      <Results results={results} />
-    </div>
-  );
+  function load() {
+    setLoaded(true);
+    search();
+  }
+
+  if (loaded) {
+    return (
+      <div className="Dictionary">
+        <section>
+          <h1>Dictionary</h1>
+          <form onSubmit={handleSubmit}>
+            <input
+              onChange={handleChange}
+              type="search"
+              placeholder="Enter your hopes and dreams..."
+            />
+          </form>
+          <div className="hint">
+            example: hippopotamus, ocean, brown bear, chocolate...
+          </div>
+        </section>
+        <Results results={results} />
+      </div>
+    );
+  } else {
+    load();
+    return "Loading";
+  }
 }
